ab5549696cb47c81f53a972f9471df6def27939f
LxL <my8100@gmail.com>
2025-01-05 17:22:36 +0800

    Support allure report

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,8 @@ import os
 from logparser.run import STAR
 from logparser.utils import check_update
 
+from tests.utils import cst
+
 
 def test_run_py():
     assert STAR  # Test importing of logparser/logparser/run.py
@@ -11,8 +13,11 @@ def test_run_py():
 
 def test_check_update():
     js = check_update(timeout=60)
+    print(js)
     if js:
         assert 'latest_version' in js and 'info' in js
+    elif cst.PY313:
+        assert js, js
     else:
         print('Got empty js.')
 
